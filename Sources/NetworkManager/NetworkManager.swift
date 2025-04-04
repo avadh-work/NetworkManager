3b629f643a67c197b54a534a1bffdb3af26957d3
Avadh Bambhroliya <avadhbambhroliya.work@gmail.com>
2025-04-04 10:57:01 +0530

    Add documentation into code

--- Sources/NetworkManager/NetworkManager.swift
+++ Sources/NetworkManager/NetworkManager.swift
@@ -4,12 +4,19 @@
 import Foundation
 import Combine
 
+/// A reusable and testable network layer class responsible for performing HTTP requests.
 @available(iOS 13.0, *)
 public class NetworkManager {
+
+    /// Singleton instance of NetworkManager.
     @MainActor public static let shared = NetworkManager()
-    
+
+    /// Private initializer to prevent external instantiation.
     private init() {}
-    
+
+    /// Fetches and decodes data from the specified URL.
+    /// - Parameter url: The URL to fetch data from.
+    /// - Returns: A publisher emitting the decoded response or an error.
     public func fetch<T: Decodable>(url: URL) -> AnyPublisher<T, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .map { $0.data }
